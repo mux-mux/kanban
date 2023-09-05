ec5ec310b4d8be54edca10dc78d79a204fd8d124
mux-mux <kmmyemail@ukr.net>
2023-09-05 21:21:14 +0300

    add updateDOM on load from localStorage or set default values

--- src/index.js
+++ src/index.js
@@ -1,16 +1,4 @@
+import { updateDOM } from './modules/updateDOM';
 import './styles/main.scss';
-
-import { getLocalItems, setLocalItems } from './modules/localStorage';
-
-let updatedOnLoad = false;
-
-function updateDOM() {
-  if (!updatedOnLoad) {
-    getLocalItems();
-  }
-
-  updatedOnLoad = true;
-  setLocalItems();
-}
 
 updateDOM();
