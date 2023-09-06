83a1830a53e23205a625f2db33e8fe97bdbc2e0a
mikemouse <kmmyemail@ukr.net>
2023-09-06 17:54:26 +0300

    add drag drop events, update lists

---
+++ src/modules/dragDropItem.js
@@ -0,0 +1,40 @@
+import { updateLists } from './localStorage';
+
+let draggedItem = null,
+  currentColumn = null;
+
+let dragList = document.querySelectorAll('.drag__list');
+
+function drag(e) {
+  draggedItem = e.target;
+}
+
+function dragEnter(column) {
+  dragList[column].classList.add('over');
+  currentColumn = column;
+}
+
+function allowDrop(e) {
+  e.preventDefault();
+}
+
+function drop(e) {
+  e.preventDefault();
+
+  dragList.forEach((list) => {
+    list.classList.remove('over');
+  });
+
+  const parent = dragList[currentColumn];
+  parent.appendChild(draggedItem);
+
+  updateLists();
+}
+
+dragList.forEach((list, i) => {
+  list.addEventListener('dragover', (e) => allowDrop(e));
+  list.addEventListener('drop', (e) => drop(e));
+  list.addEventListener('dragenter', () => dragEnter(i));
+});
+
+export { drag, allowDrop, drop };
