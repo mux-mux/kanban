e1372b27c1fdc72785fd86911ba680349b4ea0c2
mux-mux <kmmyemail@ukr.net>
2023-09-08 22:23:52 +0300

    add better UI of editing deleting item

--- src/modules/createItem.js
+++ src/modules/createItem.js
@@ -1,16 +1,53 @@
 import { drag } from './dragDropItem';
 import { toggleInputBox } from './addItem';
-import { updateItem } from './updateItem';
 
 function createItem(columnElement, columnNum, item, itemNum) {
   const listElement = document.createElement('li');
   listElement.classList.add('drag__list-item');
   listElement.textContent = item;
   listElement.draggable = true;
-  listElement.contentEditable = true;
   listElement.id = itemNum;
   listElement.addEventListener('dragstart', (e) => drag(e));
-  listElement.addEventListener('focusout', (e) => updateItem(itemNum, columnNum));
+
+  hoverAppearIcon(listElement);
+  dblclickEditable(listElement);
+
   columnElement.appendChild(listElement);
 }
+
+function hoverAppearIcon(currentelement) {
+  currentelement.addEventListener('mouseover', (e) =>
+    e.target.style.setProperty('--display', 'initial')
+  );
+  currentelement.addEventListener('mouseout', (e) =>
+    e.target.style.setProperty('--display', 'none')
+  );
+}
+
+function dblclickEditable(currentelement) {
+  currentelement.addEventListener('dblclick', (e) => {
+    e.target.setAttribute('contentEditable', true);
+    focusEnterEnd(e);
+  });
+  currentelement.addEventListener('focusout', (e) =>
+    e.target.setAttribute('contentEditable', false)
+  );
+}
+
+function focusEnterEnd(ev) {
+  ev.target.focus();
+  const range = document.createRange();
+  range.selectNodeContents(ev.target);
+  range.collapse(false);
+  const selection = window.getSelection();
+  selection.removeAllRanges();
+  selection.addRange(range);
+  ev.target.addEventListener('keypress', function (event) {
+    if (event.key === 'Enter') {
+      event.preventDefault();
+      ev.target.blur();
+    }
+  });
+}
+
 export { createItem };
