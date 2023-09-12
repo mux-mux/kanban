17790567c27907b91544d70b9c23b379f17a9109
mikemouse <kmmyemail@ukr.net>
2023-09-12 13:57:15 +0300

    add deleteItem functionality remove console

--- src/modules/deleteItem.js
+++ src/modules/deleteItem.js
@@ -3,9 +3,7 @@ import { updateDOM } from './updateDOM';
 
 function deleteItem(columnNum, itemNum) {
   const selectedList = itemsList[columnNum];
-  console.log(selectedList);
   selectedList.splice(itemNum, 1);
-  console.log(selectedList.splice(itemNum, 1));
 
   updateDOM();
 }
