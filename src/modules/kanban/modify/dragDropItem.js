4b4ca47d1ac3190aff5ff703fbcbb41a2f40cd75
mikemouse <kmmyemail@ukr.net>
2023-09-21 14:25:55 +0300

    add 1-st version of sortable lists

--- src/modules/kanban/modify/dragDropItem.js
+++ src/modules/kanban/modify/dragDropItem.js
@@ -6,11 +6,22 @@ let draggedItem = null,
 
 function drag(e) {
   draggedItem = e.currentTarget;
+
+  e.dataTransfer.setData('text/html', draggedItem.innerHTML);
   draggedItem.style.setProperty('--display', 'none');
 }
 
 function dragEnter(e, column) {
   e.preventDefault();
+
+  const target = e.target;
+  if (target && target !== draggedItem && target.nodeName === 'LI') {
+    dragList[currentColumn].insertBefore(draggedItem, target.nextSibling);
+  }
+  if (target.childElementCount === 0) {
+    dragList[currentColumn].appendChild(draggedItem);
+  }
+
   dragList[column].classList.add('over');
   currentColumn = column;
 }
@@ -22,12 +33,12 @@ function dragLeave(column) {
 function drop(e) {
   e.preventDefault();
 
+  draggedItem.innerHTML = e.dataTransfer.getData('text/html');
+  draggedItem = null;
+
   dragList.forEach((list) => {
     list.classList.remove('over');
   });
-
-  const parent = dragList[currentColumn];
-  parent.appendChild(draggedItem);
 
   onDragUpdate();
 }
