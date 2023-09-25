28c4686696d88688a8920ec1f2cd2a9756223834
mikemouse <kmmyemail@ukr.net>
2023-09-25 16:22:26 +0300

    make deadline picker onreload, onedit - persistent

--- src/modules/kanban/set/deadline.js
+++ src/modules/kanban/set/deadline.js
@@ -1,6 +1,35 @@
+import { elementWithClass } from './createItem';
+import { localLoaded, updateDOM } from '../update/updateDOM';
+import { columnNames } from '../data/columns';
+
 function minDeadline() {
-  const today = new Date().toISOString().split('T')[0];
-  return today;
+  const todayDate = new Date().toISOString().split('T')[0];
+  return todayDate;
 }
 
-export { minDeadline };
+function setDeadline(columnNum, item, itemNum) {
+  const deadlinePick = elementWithClass('input', 'deadline');
+
+  const today = minDeadline();
+  let currDaedline = localLoaded[columnNames[columnNum]].items[itemNum].deadline;
+
+  today > currDaedline && columnNum !== 2
+    ? (deadlinePick.style.color = '#d02020')
+    : (deadlinePick.style.color = '#777');
+  deadlinePick.setAttribute('type', 'date');
+  deadlinePick.setAttribute('min', today);
+  deadlinePick.value = item.deadline;
+
+  deadlinePick.addEventListener('change', (e) => {
+    localLoaded[columnNames[columnNum]].items[itemNum].deadline = e.currentTarget.value;
+    updateDOM();
+  });
+
+  if (columnNum === 2) {
+    deadlinePick.setAttribute('disabled', 'disabled');
+  }
+
+  return deadlinePick;
+}
+
+export { minDeadline, setDeadline };
