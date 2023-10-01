b6aad9c49f980dffc763ad26ccf998787dfd0710
mux-mux <kmmyemail@ukr.net>
2023-10-01 18:25:12 +0300

    add reset pomodoro on delete

--- src/modules/kanban/modify/deleteItem.js
+++ src/modules/kanban/modify/deleteItem.js
@@ -2,6 +2,7 @@ import { localLoaded } from '../update/updateDOM';
 import { columnNames } from '../data/columns';
 import { undoItem } from './undoItem';
 import { updateDOM } from '../update/updateDOM';
+import { interval } from '../set/pomodoro';
 
 const overallRemoved = [];
 
@@ -9,6 +10,8 @@ function deleteItem(columnNum, itemNum) {
   const selectedList = localLoaded[columnNames[columnNum]];
   const currRemoved = selectedList.items.splice(itemNum, 1);
   overallRemoved.push(`${JSON.stringify(currRemoved)}, ${columnNum}`);
+  clearInterval(interval);
+  console.log(currRemoved);
 
   updateDOM();
 }
