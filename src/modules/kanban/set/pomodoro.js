9152f7fb85d073c5aaff5a85d8ad902f3583499c
mikemouse <kmmyemail@ukr.net>
2023-10-06 10:41:36 +0300

    fix pomodoro reset done error

--- src/modules/kanban/set/pomodoro.js
+++ src/modules/kanban/set/pomodoro.js
@@ -24,32 +24,61 @@ function startPomodoro(duration, timer, columnNum, itemNum) {
   if (itemData.break) {
     pomodoroBreak.style.display = 'block';
   }
-  pause.addEventListener('click', () => {
+
+  pause.addEventListener('click', pausePomodoro);
+
+  function pausePomodoro() {
     pause.style.display = 'none';
     play.style.display = 'inline-block';
     isPause = true;
     timer.pomodoro.classList.remove('fa-fade');
-  });
+  }
 
-  play.addEventListener('click', () => {
+  play.addEventListener('click', playPomodoro);
+
+  function playPomodoro() {
     play.style.display = 'none';
     pause.style.display = 'inline-block';
     isPause = false;
     timer.pomodoro.classList.add('fa-fade');
-  });
+  }
 
-  reset.addEventListener('click', () => {
+  reset.addEventListener('click', resetPomodoro);
+
+  function resetPomodoro() {
     pomodoroInit(timer, itemData, 'remove', columnNum, itemNum);
 
     if (itemData.break === true) {
       itemData.break = false;
       pomodoroBreak.style.display = 'none';
     }
-  });
+    removeControlsListiners(
+      play,
+      pause,
+      reset,
+      done,
+      playPomodoro,
+      pausePomodoro,
+      resetPomodoro,
+      donePomodoro
+    );
+  }
 
-  done.addEventListener('click', () => {
+  done.addEventListener('click', donePomodoro);
+
+  function donePomodoro() {
     relocateItem(columnNum, itemNum, 2, localLoaded[columnNames[2]].items.length);
-  });
+    removeControlsListiners(
+      play,
+      pause,
+      reset,
+      done,
+      playPomodoro,
+      pausePomodoro,
+      resetPomodoro,
+      donePomodoro
+    );
+  }
 
   function pomodoroLogic() {
     const minutes = Math.floor(tick / 60);
@@ -122,6 +151,22 @@ function setPomodoro(columnNum, itemNum) {
   }
 
   return { pomodoro, lunchPomodoro };
+}
+
+function removeControlsListiners(
+  play,
+  pause,
+  reset,
+  done,
+  playPomodoro,
+  pausePomodoro,
+  resetPomodoro,
+  donePomodoro
+) {
+  done.removeEventListener('click', donePomodoro);
+  reset.removeEventListener('click', resetPomodoro);
+  pause.removeEventListener('click', pausePomodoro);
+  play.removeEventListener('click', playPomodoro);
 }
 
 export { setPomodoro, startPomodoro, interval };
