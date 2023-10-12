21464cec69ca1d9529dd76fd09507bb80b2e019c
mikemouse <kmmyemail@ukr.net>
2023-10-12 14:16:57 +0300

    fix deleteItem pomodoro control listeners issue

--- src/modules/kanban/modify/deleteItem.js
+++ src/modules/kanban/modify/deleteItem.js
@@ -7,6 +7,11 @@ import { undoItem } from './undoItem';
 const overallRemoved = [];
 
 function deleteItem(columnNum, itemNum) {
+  const pomodoroContainer = document.getElementById('pomodoro');
+  const pomodoroContainerClone = pomodoroContainer.cloneNode(true);
+
+  pomodoroContainer.parentNode.replaceChild(pomodoroContainerClone, pomodoroContainer);
+
   const selectedList = localLoaded[columnNames[columnNum]];
   const currRemoved = selectedList.items.splice(itemNum, 1);
   overallRemoved.push(`${JSON.stringify(currRemoved)}, ${columnNum}`);
