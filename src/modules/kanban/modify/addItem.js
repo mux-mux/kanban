627ee56ed552d2fbf673b9d62773fbc74620c9f1
mux-mux <kobylyukh@gmail.com>
2024-06-13 17:19:25 +0300

    fix naming of new task variables

--- src/modules/kanban/modify/addItem.js
+++ src/modules/kanban/modify/addItem.js
@@ -4,46 +4,46 @@ import { todayDate } from '../set/deadline';
 
 const buttonsOpenTask = document.querySelectorAll('.add__btn-open');
 const buttonsSaveTask = document.querySelectorAll('.add__btn-save');
-const containersAddNewTask = document.querySelectorAll('.add__container');
-const textareaAddNewTask = document.querySelectorAll('.add__item');
+const containersNewTask = document.querySelectorAll('.add__container');
+const textareasNewTask = document.querySelectorAll('.add__item');
 const taskLists = document.querySelectorAll('.tasks__list');
 
 const handleKeypress = (e, column) => {
   if (e.ctrlKey && e.code === 'Enter') {
-    if (textareaAddNewTask[column].value.length !== 0) {
-      textareaAddNewTask[column].value = textareaAddNewTask[column].value + '\n';
+    if (textareasNewTask[column].value.length !== 0) {
+      textareasNewTask[column].value = textareasNewTask[column].value + '\n';
     }
   } else if (e.code === 'Enter') {
     e.preventDefault();
     addToColumn(column);
   } else if (e.code === 'Escape') {
     toggleInputBox(column, null);
-    textareaAddNewTask[column].value = '';
+    textareasNewTask[column].value = '';
   }
 };
 
 function toggleInputBox(column, state) {
-  const addVisibility = state === 'open' ? 'hidden' : 'visible';
-  const saveVisibility = state === 'open' ? 'visible' : 'hidden';
-  const addDisplay = state === 'open' ? 'block' : 'none';
+  const buttonOpenVisibility = state === 'open' ? 'hidden' : 'visible';
+  const buttonSaveVisibility = state === 'open' ? 'visible' : 'hidden';
+  const containerDisplay = state === 'open' ? 'block' : 'none';
 
-  buttonsOpenTask[column].style.visibility = addVisibility;
-  buttonsSaveTask[column].style.visibility = saveVisibility;
-  containersAddNewTask[column].style.display = addDisplay;
+  buttonsOpenTask[column].style.visibility = buttonOpenVisibility;
+  buttonsSaveTask[column].style.visibility = buttonSaveVisibility;
+  containersNewTask[column].style.display = containerDisplay;
 
   if (state === 'save') {
     addToColumn(column);
   } else if (state === 'open') {
-    textareaAddNewTask[column].focus();
-    containersAddNewTask[column].scrollIntoView({ block: 'end' });
+    textareasNewTask[column].focus();
+    containersNewTask[column].scrollIntoView({ block: 'end' });
   }
 }
 
 function addToColumn(column) {
-  if (textareaAddNewTask[column].value === '') {
+  if (textareasNewTask[column].value === '') {
     return;
   }
-  const itemText = textareaAddNewTask[column].value;
+  const itemText = textareasNewTask[column].value;
   const selectedList = columnNames[column];
   itemText.trim().length > 0
     ? localLoaded[selectedList].items.push({
@@ -57,19 +57,19 @@ function addToColumn(column) {
         done: column === 2 ? todayDate() : '',
       })
     : null;
-  textareaAddNewTask[column].value = '';
+  textareasNewTask[column].value = '';
   updateDOM();
 }
 
-buttonsOpenTask.forEach((addBtn, index) => {
-  addBtn.addEventListener('click', () => toggleInputBox(index, 'open'));
+buttonsOpenTask.forEach((openBtn, index) => {
+  openBtn.addEventListener('click', () => toggleInputBox(index, 'open'));
 });
 
 buttonsSaveTask.forEach((saveBtn, index) => {
   saveBtn.addEventListener('click', () => toggleInputBox(index, 'save'));
 });
 
-containersAddNewTask.forEach((taskField, index) => {
+containersNewTask.forEach((taskField, index) => {
   taskField.addEventListener('keydown', (e) => handleKeypress(e, index));
 });
 
