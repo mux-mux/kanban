52e25b59a64cd569f47985273c77b9427ae4c3e8
mux-mux <kobylyukh@gmail.com>
2025-01-21 21:12:37 +0200

    fix focus-trap error on pomodoro active

--- src/modules/spinner/removeSpinner.js
+++ src/modules/spinner/removeSpinner.js
@@ -1,10 +1,7 @@
 function removeSpinner() {
-  const appRoot = document.getElementById('app');
   const appLoader = document.getElementById('app-loader');
 
   setTimeout(() => appLoader.remove(), 500);
-  appRoot.style.visibility = 'visible';
-  appRoot.style.opacity = '1';
 }
 
 export default removeSpinner;
