d6d7f1f777fc4a0c58cf115c4d0f560beb67c35e
mux-mux <kobylyukh@gmail.com>
2025-01-26 14:17:11 +0200

    refactor pomodoro logic

--- src/modules/set/pomodoro.js
+++ src/modules/set/pomodoro.js
@@ -23,63 +23,61 @@ function startPomodoro(duration, timer, columnNum, itemNum) {
 
   const MM = document.getElementById('minutes');
   const SS = document.getElementById('seconds');
-
   const pomodoroBreak = document.querySelector('.pomodoro__break');
+
   const itemsLoaded = getLocalItems();
-  let pomodoroIntervalTick = getLocalData('pomodoroInterval');
   const itemData = itemsLoaded[Object.keys(itemsLoaded)[columnNum]].items[itemNum];
   const itemInCol = document.querySelectorAll(`[data-in-col="${columnNum}"]`)[itemNum];
   const progressBar = itemInCol.querySelector('.task__progressbar');
 
   let tick = duration * 60;
+  let pomodoroIntervalTick = getLocalData('pomodoroInterval');
+
+  updateUI();
+
+  function updateUI() {
+    const minutes = String(Math.floor(tick / 60)).padStart(2, '0');
+    const seconds = String(tick % 60).padStart(2, '0');
+    const staticTime = itemData.break ? 5 : 25;
+    MM.textContent = minutes;
+    SS.textContent = seconds;
+
+    const progressPercent = Math.min(100 - (tick / (staticTime * 60)) * 100, 100);
+    progressBar.style.width = `${progressPercent}%`;
+  }
 
   if (itemData.break) {
     pomodoroBreak.style.display = 'block';
   }
 
-  function pomodoroLogic() {
-    const isPaused = getLocalData('isPaused');
-    let minutes = Math.floor(tick / 60);
-    let seconds = tick % 60;
-
-    minutes = minutes < 10 ? `0${minutes}` : minutes;
-    seconds = seconds < 10 ? `0${seconds}` : seconds;
-
-    if (tick <= 0) {
-      clearInterval(pomodoroIntervalTick);
-      removeLocalData('pomodoroInterval');
-      if (!itemData.break) {
-        itemData.sessions++;
-      }
-
-      itemData.break = !itemData.break;
-      setLocalItems(itemsLoaded);
-
-      if (itemData.break) {
-        pomodoroBreak.style.display = 'block';
-        startPomodoro(5, timer, columnNum, itemNum);
-      } else {
-        pomodoroBreak.style.display = 'none';
-        startPomodoro(25, timer, columnNum, itemNum);
-      }
-      updateDOM();
-    }
-    if (isPaused === false) {
-      timer.pomodoro.classList.add('fa-fade');
-      tick--;
-      MM.textContent = minutes;
-      SS.textContent = seconds;
-    }
-    if (itemData.pomodoro === true && isPaused === false) {
-      itemData.time = `${minutes}:${seconds}`;
-      setLocalItems(itemsLoaded);
-    }
-
-    const progressPercent = Math.min(100 - (tick / (25 * 60)) * 100, 100);
-    progressBar.style.width = `${progressPercent}%`;
+  function handleCompletion() {
+    clearInterval(pomodoroIntervalTick);
+    removeLocalData('pomodoroInterval');
+    itemData.sessions += itemData.break ? 0 : 1;
+    itemData.break = !itemData.break;
+    pomodoroBreak.style.display = itemData.break ? 'block' : 'none';
+    itemData.time = itemData.break ? '00:30' : '01:00';
+    setLocalItems(itemsLoaded);
+    updateDOM();
   }
 
-  pomodoroLogic();
+  function pomodoroLogic() {
+    const isPaused = getLocalData('isPaused');
+
+    if (!isPaused) {
+      timer.pomodoro.classList.add('fa-fade');
+      tick--;
+      updateUI();
+
+      if (itemData.pomodoro === true) {
+        itemData.time = `${MM.textContent}:${SS.textContent}`;
+        setLocalItems(itemsLoaded);
+      }
+      if (tick <= 0) {
+        handleCompletion();
+      }
+    }
+  }
 
   if (pomodoroIntervalTick) {
     clearInterval(pomodoroIntervalTick);
@@ -87,6 +85,8 @@ function startPomodoro(duration, timer, columnNum, itemNum) {
   }
   pomodoroIntervalTick = setInterval(pomodoroLogic, 1000);
   setLocalData('pomodoroInterval', pomodoroIntervalTick);
+
+  updateUI();
 }
 
 function createPomodoroStartIcon(columnNum, itemNum) {
@@ -165,7 +165,7 @@ function pomodoroInit(timer, itemData, state, columnNum, itemNum) {
 
   icon.removeEventListener('click', timer.startPomodoroByIcon);
 
-  let time = itemData.time === '' ? ['25', '00'] : itemData.time.split(':');
+  let time = itemData.time === '' ? ['01', '00'] : itemData.time.split(':');
 
   if (state === 'init') {
     isPaused ? pausePomodoro() : playPomodoro();
@@ -187,7 +187,6 @@ function pomodoroInit(timer, itemData, state, columnNum, itemNum) {
     removeLocalData('pomodoroInterval');
     itemData.pomodoro = false;
     itemData.time = '';
-    time = ['25', '00'];
 
     icon.style.cssText = 'opacity: var(--opacity); pointer-events: var(--pointer-events);';
     icon.classList.remove('fa-fade');
