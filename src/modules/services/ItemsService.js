2fb3299c78975d93485411223edd843b5122231d
mux-mux <kobylyukh@gmail.com>
2025-01-27 09:16:58 +0200

    add throw an error on fetch response not ok status

--- src/modules/services/ItemsService.js
+++ src/modules/services/ItemsService.js
@@ -1,16 +1,23 @@
 async function getItems(url) {
+  const headers = {
+    'X-Access-Key': '$2b$10$wh.K0go7oYuH93pP1amFBupx4yjy0VcOnlcWDM.Oh9CT4o1QxvuFq',
+    'X-Bin-Meta': 'false',
+  };
+
   try {
-    const res = await fetch(url, {
+    const response = await fetch(url, {
       method: 'GET',
-      headers: {
-        'X-Access-Key': '$2b$10$wh.K0go7oYuH93pP1amFBupx4yjy0VcOnlcWDM.Oh9CT4o1QxvuFq',
-        'X-Bin-Meta': false,
-      },
+      headers,
     });
-    return await res.json();
+
+    if (!response.ok) {
+      throw new Error(`Error: ${response.status} ${response.statusText}`);
+    }
+
+    return await response.json();
   } catch (error) {
-    alert('There was an issue with fetching data');
-    return error;
+    alert('There was an issue with fetching data. Please try again later.');
+    return null;
   }
 }
 
