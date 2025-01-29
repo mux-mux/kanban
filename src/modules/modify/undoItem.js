7a628de4aaa4ab5e88e5f9d0739783cca4e1234e
mux-mux <kobylyukh@gmail.com>
2025-01-29 09:02:01 +0200

    refactor undoItem code structure

--- src/modules/modify/undoItem.js
+++ src/modules/modify/undoItem.js
@@ -1,17 +1,20 @@
+import checkFunctionParameters from '../errors/checkFunctionParameters';
 import updateDOM from '../update/updateDOM';
 import { getLocalItems, setLocalItems, getLocalData } from '../update/localStorage';
 import { setSessionRemovedItems, getSessionRemovedItems } from '../update/sessionStorage';
 
-function undoItem(columnNames, removed) {
+function undoItem(columnNames, removedItems) {
+  checkFunctionParameters(columnNames, removedItems);
+
   const itemsLoaded = getLocalItems();
-  const getBackItem = removed.pop();
-  const [item, column] = getBackItem;
-  const selectedList = columnNames[column];
+  const [item, columnIndex] = removedItems.pop();
+  const columnName = columnNames[columnIndex];
 
-  itemsLoaded[selectedList].items.push(item);
-  setSessionRemovedItems(removed);
+  if (!itemsLoaded[columnName]) return;
+
+  itemsLoaded[columnName].items.push(item);
+  setSessionRemovedItems(removedItems);
   setLocalItems(itemsLoaded);
-
   updateDOM();
 }
 
@@ -19,11 +22,11 @@ function setUndoListeners() {
   const buttonUndoDelete = document.querySelector('.tool-history');
 
   function handleUndoItem() {
-    const itemsRemoved = getSessionRemovedItems();
+    const removedItems = getSessionRemovedItems();
     const columnNames = getLocalData('columnNames');
 
-    if (itemsRemoved.length !== 0) {
-      undoItem(columnNames, itemsRemoved);
+    if (removedItems.length !== 0) {
+      undoItem(columnNames, removedItems);
     }
   }
 
@@ -33,9 +36,7 @@ function setUndoListeners() {
     }
   });
 
-  buttonUndoDelete.addEventListener('click', () => {
-    handleUndoItem();
-  });
+  buttonUndoDelete.addEventListener('click', handleUndoItem);
 }
 
 export default setUndoListeners;
