a5319ecad8b7a37e5c6619a074956f1ad3516a51
mux-mux <kobylyukh@gmail.com>
2025-01-31 13:35:11 +0200

    refactor deleteItem code structure

--- src/modules/components/task/delete.js
+++ src/modules/components/task/delete.js
@@ -17,53 +17,67 @@ import {
 import { setSessionRemovedItems, getSessionRemovedItems } from '../../update/sessionStorage';
 
 function deleteItem(type = 'task', columnNum = 0, itemNum = 0) {
-  const pomodoroIntervalTick = getLocalData('pomodoroInterval');
   if (type === 'task') {
+    delteItemType('task', columnNum, itemNum);
+  } else if (type === 'category') {
+    delteItemType('category', columnNum);
+  }
+  updateDOM();
+}
+
+function delteItemType(type, columnNum, itemNum = 0) {
+  if (type === 'task') {
+    removePomodoroTimerListiners();
     const itemsLoaded = getLocalItems();
     const itemsRemoved = getSessionRemovedItems();
-    removePomodoroTimerListiners();
 
     const selectedList = itemsLoaded[Object.keys(itemsLoaded)[columnNum]];
-    const currRemoved = selectedList.items.splice(itemNum, 1);
-    itemsRemoved.push([...currRemoved, columnNum]);
+    const removedItem = selectedList.items.splice(itemNum, 1);
+    itemsRemoved.push([...removedItem, columnNum]);
 
     setSessionRemovedItems(itemsRemoved);
     setLocalItems(itemsLoaded);
+
+    const pomodoroIntervalTick = getLocalData('pomodoroInterval');
     clearInterval(pomodoroIntervalTick);
     removeLocalData('pomodoroInterval');
   } else if (type === 'category') {
     const categoriesLoaded = getLocalData('categoriesItems');
     categoriesLoaded.splice(columnNum, 1);
-
     setLocalData('categoriesItems', categoriesLoaded);
   }
-
-  updateDOM();
 }
 
 function createDeleteIcon(type, columnNum, itemNum = 0) {
   checkFunctionParameters(type, columnNum);
 
-  const taskDeleteButton = createElementWithClass('button', ['icon', 'icon-delete']);
-  const taskDeleteIcon = createElementWithClass('i', ['fa-solid', 'fa-x']);
+  const deleteButton = createElementWithClass('button', ['icon', 'icon-delete']);
+  deleteButton.appendChild(createElementWithClass('i', ['fa-solid', 'fa-x']));
 
+  const ariaLabel = getDeleteAriaLabel(type, columnNum, itemNum);
+  if (ariaLabel) {
+    deleteButton.ariaLabel = ariaLabel;
+  }
+
+  if (!isTouchDevice()) {
+    deleteButton.addEventListener('focus', (e) => toggleItemIconOpacity(e, 1));
+    deleteButton.addEventListener('blur', (e) => toggleItemIconOpacity(e, 0));
+  }
+
+  return deleteButton;
+}
+
+function getDeleteAriaLabel(type, columnNum, itemNum) {
   if (type === 'task') {
     const itemsLoaded = getLocalItems();
-    const taskText = itemsLoaded[Object.keys(itemsLoaded)[columnNum]].items[itemNum].name;
-    taskDeleteButton.ariaLabel = `Delete the ${taskText} task`;
+    const taskText = itemsLoaded[Object.keys(itemsLoaded)[columnNum]].items[itemNum]?.name;
+    return taskText ? `Delete the ${taskText} task` : null;
   } else if (type === 'category') {
     const categoriesLoaded = getLocalData('categoriesItems');
     const categoryText = categoriesLoaded[columnNum];
-    taskDeleteButton.ariaLabel = `Delete the ${categoryText} category`;
+    return categoryText ? `Delete the ${categoryText} category` : null;
   }
-
-  taskDeleteButton.appendChild(taskDeleteIcon);
-
-  !isTouchDevice() &&
-    taskDeleteButton.addEventListener('focus', (e) => toggleItemIconOpacity(e, 1));
-  !isTouchDevice() && taskDeleteButton.addEventListener('blur', (e) => toggleItemIconOpacity(e, 0));
-
-  return taskDeleteButton;
+  return null;
 }
 
 export { deleteItem, createDeleteIcon };
